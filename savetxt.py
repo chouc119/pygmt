d54dd3618679c54b7beebed71fce9df27baa6a59
chouu <101647060+chouc119@users.noreply.github.com>
2022-05-03 20:23:59 +0800

    Update savetxt.py

--- savetxt.py
+++ savetxt.py
@@ -1,8 +1,8 @@
+#chou
 import pygmt as gmt     
 import numpy as np        
 
 TWgrid = gmt.datasets.load_earth_relief(resolution="15s",region=[119, 123, 21, 26])                
-
 
 #max=np.max(TWgrid)
 #min=np.min(TWgrid)
